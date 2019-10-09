58c76d80ef545832da49d5c5ebfec8eac2b12e29
Niklas Nauber <nauber91@hotmail.com>
2019-10-09 11:48:43 +0200

    Includes media queries in theme

--- lib/index.tsx
+++ lib/index.tsx
@@ -2,23 +2,15 @@ import React, { FC } from "react";
 import styled, { keyframes } from "styled-components";
 import DropDown from "../src/components/DropDown";
 
-const animations = {
-  fadeIn: keyframes`
-    0% {
-      opacity: 0;
-    }
-    100% {
-      opacity: 1;
-    }`,
-  fadeInAndExpand: keyframes`
-    0% {
-      opacity: 0;
-      max-height: 0;
-    }
-    100% {
-      opacity: 1;
-      max-height: 900px
-    }`
+// Media queries
+const mqSizes = {
+  mobileS: "320px",
+  mobileM: "375px",
+  mobileL: "425px",
+  tablet: "768px",
+  laptop: "1024px",
+  laptopL: "1440px",
+  desktop: "2560px"
 };
 
 const theme = {
@@ -42,7 +34,35 @@ const theme = {
     orange: {
       main: "orange"
     }
+  },
+  animations: {
+    fadeIn: keyframes`
+      0% {
+        opacity: 0;
+      }
+      100% {
+        opacity: 1;
+      }`,
+    fadeInAndExpand: keyframes`
+      0% {
+        opacity: 0;
+        max-height: 0;
+      }
+      100% {
+        opacity: 1;
+        max-height: 900px
+      }`
+  },
+  mediaQueries: {
+    mobileS: (limit: string) => `(${limit}-width: ${mqSizes.mobileS})`,
+    mobileM: (limit: string) => `(${limit}-width: ${mqSizes.mobileM})`,
+    mobileL: (limit: string) => `(${limit}-width: ${mqSizes.mobileL})`,
+    tablet: (limit: string) => `(${limit}-width: ${mqSizes.tablet})`,
+    laptop: (limit: string) => `(${limit}-width: ${mqSizes.laptop})`,
+    laptopL: (limit: string) => `(${limit}-width: ${mqSizes.laptopL})`,
+    desktop: (limit: string) => `(${limit}-width: ${mqSizes.desktop})`,
+    desktopL: (limit: string) => `(${limit}-width: ${mqSizes.desktop})`
   }
 };
 
-export { DropDown, theme, animations };
+export { DropDown, theme };
